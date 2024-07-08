61a06bee9272b8d6dc6eba43ebe1a26c55545c8d
Yaroslav <yaroslav@keyoneinc.uk>
2024-07-08 04:07:03 +0100

    added some test code in main.cpp, all works

--- hw20-2/main.cpp
+++ hw20-2/main.cpp
@@ -1,5 +1,19 @@
 #include <iostream>
+#include "Airplane.h"
 
 int main() {
 
+	Airplane airplane(1709, "Krakow Airport (KRK)", "Dublin Airport (DUB)", "Ryanair Plane", 160);
+
+	airplane.addPassenger("Yaroslav");
+	airplane.addPassenger("Danylo");
+	airplane.addPassenger("Mykola");
+
+	airplane.printAirplaneInfo();
+
+	airplane.removePassenger("Danylo");
+
+	airplane.printAirplaneInfo();
+
+	return 0;
 }
